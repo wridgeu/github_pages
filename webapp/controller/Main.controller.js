45727ff5b3ba1860b1c35858cb8c7e45aea92b86
Marco Beier <marco-beier@t-online.de>
2020-01-24 20:36:45 +0100

    added quite a few changes, svg logos for socials, href attributes

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function(Controller) {
+  "sap/ui/core/mvc/Controller",
+  'sap/ui/model/json/JSONModel'
+], function(Controller, JSONModel) {
   "use strict";
-
   return Controller.extend("sapmarco.projectpages.controller.Main", {
-    
+    onSocialPress(oEvt, url){
+      window.open(url, '_blank')
+    }
   });
 });
