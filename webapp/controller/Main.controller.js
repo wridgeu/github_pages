f8cb364b0d6c99f73a1df4738dc3a3708abb322d
Marco Beier <marco-beier@t-online.de>
2020-04-01 20:39:13 +0200

    chore: pretty print on code

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,18 +1,18 @@
 sap.ui.define([
-  "./Base"
-], function(BaseController) {
-  "use strict";
-  return BaseController.extend("sapmarco.projectpages.controller.Main", {
-    onInit: function(){
-      //set content density
-      this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-      this.initializeViewTheme();
-    },
-    onUI5IconPress : function () {
+	"./Base",
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("sapmarco.projectpages.controller.Main", {
+		onInit: function () {
+			//set content density
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+			this.initializeViewTheme();
+		},
+		onUI5IconPress: function () {
 			this.getOwnerComponent().openVersionDialog();
-    },
-    onThemeSwap: function(sTheme){
-      this.toggleTheme(sTheme);
-    },
-  });
+		},
+		onThemeSwap: function (sTheme) {
+			this.toggleTheme(sTheme);
+		},
+	});
 });
--- webapp/controller/VersionDialog.js
+++ webapp/controller/VersionDialog.js
@@ -5,39 +5,39 @@ sap.ui.define([
 ], function (ManagedObject, Fragment, syncStyleClass) {
 	"use strict";
 	return ManagedObject.extend("sapmarco.projectpages.controller.VersionDialog", {
-		constructor : function (oView) {
+		constructor: function (oView) {
 			this._oView = oView;
 		},
-		exit : function () {
+		exit: function () {
 			delete this._oView;
 		},
-		open : function () {
+		open: function () {
 			var oView = this._oView;
 
 			// create dialog lazily
 			if (!oView.byId("VersionDialog")) {
 				var oFragmentController = {
-					onCloseDialog : function (oEvt) {
+					onCloseDialog: function (oEvt) {
 						oEvt.getSource().getParent().close();
 						oEvt.getSource().getParent().getModel("versionInfo").destroy();
 					}
 				};
 				// load asynchronous XML fragment
-				Fragment.load({ 
-                    type: "XML",
+				Fragment.load({
+					type: "XML",
 					id: oView.getId(),
 					name: "sapmarco.projectpages.view.VersionDialog",
 					controller: oFragmentController
 				}).then(function (oDialog) {
 					// connect dialog to the root view of this component (models, lifecycle)
-                    oView.addDependent(oDialog)
-                    sap.ui.require(['sap/ui/VersionInfo'], function(oVersInfo) {
+					oView.addDependent(oDialog)
+					sap.ui.require(['sap/ui/VersionInfo'], function (oVersInfo) {
 						oVersInfo.load()
-							.then(function(oVersion){
+							.then(function (oVersion) {
 								var oVersionData = new sap.ui.model.json.JSONModel(oVersion);
 								oDialog.setModel(oVersionData, "versionInfo");
 							});
-                    });                    
+					});
 					// forward compact/cozy style into dialog
 					syncStyleClass(oView.getController().getOwnerComponent().getContentDensityClass(), oView, oDialog);
 					oDialog.open();
