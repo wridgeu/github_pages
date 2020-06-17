37c6e5c6a1729e8964a97953546e25df9a5b0cf2
Marco Beier <marco-beier@t-online.de>
2020-06-17 20:19:10 +0200

    added footer and some init logic - should be overhauled later

--- webapp/controller/Wiki.controller.js
+++ webapp/controller/Wiki.controller.js
@@ -4,13 +4,23 @@ sap.ui.define(["./Base", "sapmarco/projectpages/libs/marked.min"], function (
 ) {
 	"use strict";
 	return BaseController.extend("sapmarco.projectpages.controller.Wiki", {
+		onInit: function () {
+			//Init
+			this.initializeViewTheme();
+			this.getView().addStyleClass(
+				this.getOwnerComponent().getContentDensityClass()
+			);
+		},
+		onThemeSwap: function (sTheme) {
+			this.toggleTheme(sTheme);
+		},
 		onBackHome: function () {
 			this.onNavBack();
 		},
 		onMarkdown: function () {
 			(async function () {
 				let response = await fetch(
-					"https://raw.githubusercontent.com/wiki/SAPMarco/SAPMarco.github.io/UI5%20CLI.md"
+					"https://raw.githubusercontent.com/wiki/SAPMarco/SAPMarco.github.io/_Sidebar.md"
 				).then((response) => response.text());
 				this.byId("markdown").getDomRef().innerHTML = Marked(response); //_xContent.innerHTML
 			}.bind(this)());
