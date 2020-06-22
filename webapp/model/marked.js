46fb38e5cdfdcc1b74635ec778f9a754d2696034
Marco Beier <marco-beier@t-online.de>
2020-06-22 21:23:53 +0200

    marked.js extension change

--- webapp/model/marked.js
+++ webapp/model/marked.js
@@ -1,27 +1,31 @@
-sap.ui.define(["sapmarco/projectpages/libs/marked.min"], function (Marked) {
+sap.ui.define(["sapmarco/projectpages/libs/marked.min", "sapmarco/projectpages/libs/highlight"], function (Marked, Highlight) {
 	"use strict";
-
+	// Syntax Highlighting
 	Marked.setOptions({
 		highlight: function (code, lang, callback) {
-			sap.ui.require(["sapmarco/projectpages/libs/prism"], function (Prism) {
-				// syntax highlighting via prism.js
-				callback(Prism.highlight(code, lang).toString());
-			});
-		},
+			// unsupported edge-cases
+			if(lang === 'i18n' || lang === '' || lang === 'url' ) return
+			return Highlight.highlightAuto(code).value;
+		}
 	});
+
+	// Image Rendering
 	const renderer = {
 		paragraph(text) {
-			//transform <p>-tags that match the regex to <img>-tags
+			// transform <p>-tags that match the regex to <img>-tags
 			const regEx = /\[\[.+?\.(?:jpg|gif|png)\]\]/g;
-			if (!text.match(regEx)) {
-				return false;
-			}
+			// doesn't match our [[<something>.jpg]]-pattern
+			if (!text.match(regEx)) return false
+			// remove whitespace (edge case)
+			text = text.trim()
+			// remove '[[' and ']]'
 			const image = text.substring(2, text.length - 2);
+			// rebuild image path for raw github
 			const imagePath = `https://raw.githubusercontent.com/wiki/SAPMarco/SAPMarco.github.io/${image}`;
 			return `
-				<img src="${imagePath}"></img>
+				<img class="wikiImage" src="${imagePath}"></img>
 			`;
-		},
+		}
 	};
 
 	Marked.use({ renderer });
