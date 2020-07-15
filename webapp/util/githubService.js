05ef402c24c4bb78e467cf529387697ae8911ecd
Marco Beier <marco-beier@t-online.de>
2020-07-15 14:37:03 +0200

    Code Cleanup

--- webapp/util/githubService.js
+++ webapp/util/githubService.js
@@ -1,14 +1,14 @@
-sap.ui.define([], function () {
+sap.ui.define([],() => {
 	"use strict";
 
 	return {
-		getSelectedContent: function(mdContent){
+		getSelectedContent(mdContent){
 			//return markdown content & encode '-' with %20
 			return fetch(
 				`https://raw.githubusercontent.com/wiki/SAPMarco/SAPMarco.github.io/${mdContent.replace(/[-*?]/g,"%20")}.md`
 			).then((response) => response.text())
 		},
-		getWikiIndex: function(){
+		getWikiIndex(){
 			//return sidebar to use as initial entry point
 			return fetch(
 				`https://raw.githubusercontent.com/wiki/SAPMarco/SAPMarco.github.io/_Sidebar.md`
