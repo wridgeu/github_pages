dd130f44416c831c4a1b74ff4a05f4748ff91bec
Marco Beier <marco-beier@t-online.de>
2022-06-25 21:38:28 +0200

    fix(rollup): bundling issue with highlightjs

--- src/util/markdownService.ts
+++ src/util/markdownService.ts
@@ -1,16 +1,31 @@
-import hljs from "highlight.js";
+import hljs from "highlight.js/lib/core";
+import js from "highlight.js/lib/languages/javascript";
+import xml from "highlight.js/lib/languages/xml";
+import css from "highlight.js/lib/languages/css";
+import shell from "highlight.js/lib/languages/shell";
+import bash from "highlight.js/lib/languages/bash";
+import json from "highlight.js/lib/languages/json";
 import { marked } from "marked";
+
+// Prevent Rollup "Unexpected token Error", some tokens within certain language definitions (i.e. arduino) throw rollup off
+// Err: "rollup-plugin-inject: failed to parse '<path-to-file.js>' Consider restricting the plugin to particular files via options.include"
+hljs.registerLanguage("javascript", js);
+hljs.registerLanguage("xml", xml);
+hljs.registerLanguage("css", css);
+hljs.registerLanguage("shell", shell);
+hljs.registerLanguage("bash", bash);
+hljs.registerLanguage("json", json);
 
 /**
  * Syntax Highlighting
  * @returns {string} highlighted code block
  */
 marked.setOptions({
-	highlight: function (string: string, lang?: string): string|void {
-		if(lang && hljs.getLanguage(lang)) {
+	highlight: function(string: string, lang?: string): string | void {
+		if (lang && hljs.getLanguage(lang)) {
 			return hljs.highlight(string, { language: lang }).value;
 		}
-	},
+	}
 });
 
 /**
@@ -29,7 +44,7 @@ const renderer = {
 		const imagePath = `https://raw.githubusercontent.com/wiki/wridgeu/wridgeu.github.io/${image}`;
 
 		return `<img class="wikiImage" src="${imagePath}"></img>`;
-	},
+	}
 };
 
 marked.use({ renderer });
